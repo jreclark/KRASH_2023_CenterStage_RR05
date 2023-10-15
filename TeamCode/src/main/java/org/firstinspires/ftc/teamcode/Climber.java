ffa27c630344f208483b3e5868e6e18c06ea5a97
John Clark <jrcxxx@gmail.com>
2023-10-15 16:15:37 -0400

    Created Climber class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Climber.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Climber {
+    private DcMotorEx leftClimb;
+    private DcMotorEx rightClimb;
+    Telemetry telemetry;
+
+    public Climber(HardwareMap hardwareMap, Telemetry telemetry){
+        this.telemetry = telemetry;
+
+    leftClimb = hardwareMap.get(DcMotorEx.class, "leftClimb");
+    rightClimb = hardwareMap.get(DcMotorEx.class,"rightclimb");
+
+    leftClimb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    rightClimb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    leftClimb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    rightClimb.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    public void runClimber(double power){
+        leftClimb.setPower(power);
+        rightClimb.setPower(power);
+    }
+}
