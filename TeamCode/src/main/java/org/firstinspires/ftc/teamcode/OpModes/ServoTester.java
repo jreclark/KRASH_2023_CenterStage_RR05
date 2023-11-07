de909c436a93371d3e222db3009a6fbd9728e6a6
John Clark <jrcxxx@gmail.com>
2023-11-07 17:47:19 -0500

    Cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ServoTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ServoTester.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -10,6 +11,7 @@ import org.firstinspires.ftc.teamcode.util.ButtonState;
 /**
  * This mode is used to tune servo positions
  */
+@Disabled
 @TeleOp(group = "Test")
 public class ServoTester extends LinearOpMode {
 
